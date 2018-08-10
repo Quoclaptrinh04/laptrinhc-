f5e3ccb56dc2c55ab8994abf91f702039649fdcd
Quoclaptrinh04 <tryonevn@gmail.com>
2018-08-10 19:07:40 +0700

    lap trinh c++

---
+++ binhphuong.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std ;
+
+int main()
+    {
+        int a;
+        cout << "nhap so a:";
+        cin >>a;
+    	if (a==0)
+    	    {
+    	    	 cout << "a binh phuong = 0";
+    	    }
+    	else if (a==0)
+            {
+			     cout << "a binh phuong = 1";
+	        }    
+		else 
+		    {
+			     cout << "a binh phuong =" << a*a;
+		    }
+	        }		     	
